54c5fe62de1836cac1f413fe41dfbf647f924664
nessup <nessup@gmail.com>
2013-05-23 14:47:10 -0700

    Added support for bold, italic, and underline

--- Notes/Note editing/Controller/EditNoteViewController.m
+++ Notes/Note editing/Controller/EditNoteViewController.m
@@ -10,16 +10,25 @@
 
 #import "UIViewController+KeyboardNotifications.h"
 
+enum {
+    TextStyleBold,
+    TextStyleItalic,
+    TextStyleUnderline
+};
+
 @interface EditNoteViewController ()
 @property (strong, nonatomic) UIPopoverController *masterPopoverController;
 
 @property (nonatomic, weak) UIWebView *webView;
 @property (nonatomic, strong) UIToolbar *editingToolbar;
+@property (nonatomic, weak) UIWindow *keyboardWindow;
 
 - (void)configureView;
 @end
 
-@implementation EditNoteViewController
+@implementation EditNoteViewController {
+    BOOL currentBoldStatus, currentItalicStatus, currentUnderlineStatus;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -35,10 +44,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    [self configureView];
     
     [self registerForKeyboardNotifications];
+    
+    [self configureView];
 }
 
 - (void)configureView
@@ -46,6 +55,8 @@
     if (self.detailItem) {
         self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
     }
+    
+    [self checkSelection:self];
 }
 
 #pragma mark - Properties
@@ -91,6 +102,20 @@
     }
 }
 
+- (UIWindow *)keyboardWindow
+{
+    if( _keyboardWindow )
+        return _keyboardWindow;
+    
+    for (UIWindow *testWindow in [[UIApplication sharedApplication] windows]) {
+        if (![[testWindow class] isEqual:[UIWindow class]]) {
+            _keyboardWindow = testWindow;
+            break;
+        }
+    }
+    
+    return _keyboardWindow;
+}
 
 #pragma mark - Layout
 
@@ -110,39 +135,114 @@
 
 - (void)keyboardWillAppear:(NSNotification *)notification
 {
+    KeyboardNotificationUserInfo keyboardInfo = [notification keyboardUserInfo];
+    
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self configureKeyboardAccessory];
+        [self removeDefaultKeyboardAccessory];
     });
+    
+    self.editingToolbar.frame = (CGRect) {
+        0.f,
+        CGRectGetMaxY(self.view.frame),
+        self.editingToolbar.frame.size
+    };
+    
+    [self.keyboardWindow addSubview:self.editingToolbar];
+    
+    [UIView animateWithDuration:keyboardInfo.animationDuration delay:0.f options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.editingToolbar.frame = CGRectMake(0.0f, CGRectGetMinY([notification keyboardUserInfo].frameEnd), self.view.frame.size.width, 44.f);
+    } completion:nil];
 }
 
-- (void)configureKeyboardAccessory {
-    // Locate non-UIWindow.
-    UIWindow *keyboardWindow = nil;
-    for (UIWindow *testWindow in [[UIApplication sharedApplication] windows]) {
-        if (![[testWindow class] isEqual:[UIWindow class]]) {
-            keyboardWindow = testWindow;
-            break;
-        }
-    }
-    
+- (void)keyboardWillDisappear:(NSNotification *)notification
+{
+    [UIView animateWithDuration:0.25 animations:^{
+        self.editingToolbar.frame = (CGRect) {
+            0.0f,
+            self.view.frame.size.height,
+            self.editingToolbar.frame.size
+        };
+    }];
+}
+
+- (void)keyboardDidDisappear:(NSNotification *)notification
+{
+    self.keyboardWindow = nil;
+}
+
+- (void)removeDefaultKeyboardAccessory {
     // Locate UIWebFormView.
-    UIView *keyboardView = nil;
-    for (UIView *possibleFormView in [keyboardWindow subviews]) {
+    for (UIView *possibleFormView in [self.keyboardWindow subviews]) {
         // iOS 5 sticks the UIWebFormView inside a UIPeripheralHostView.
         if ([[possibleFormView description] rangeOfString:@"UIPeripheralHostView"].location != NSNotFound) {
             for (UIView *subview in [possibleFormView subviews]) {
-                if( [NSStringFromClass([subview class]) isEqualToString:@"UIKeyboardAutomatic"] ) {
-                    keyboardView = subview;
-                }
-                
                 if ([[subview description] rangeOfString:@"UIWebFormAccessory"].location != NSNotFound) {
                     [subview removeFromSuperview];
                 }
             }
         }
     }
+}
+
+- (void)checkSelection:(id)sender {
+    BOOL boldEnabled = [[self.webView stringByEvaluatingJavaScriptFromString:@"document.queryCommandState('Bold')"] boolValue];
+    BOOL italicEnabled = [[self.webView stringByEvaluatingJavaScriptFromString:@"document.queryCommandState('Italic')"] boolValue];
+    BOOL underlineEnabled = [[self.webView stringByEvaluatingJavaScriptFromString:@"document.queryCommandState('Underline')"] boolValue];
     
-    [keyboardView addSubview:self.editingToolbar];
+    NSArray *segItemsArray = [NSArray arrayWithObjects:
+                              (boldEnabled) ? @"[B]" : @"B",
+                              italicEnabled ? @"[I]" : @"I",
+                              underlineEnabled ? @"[U]" : @"U", nil];
+    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:segItemsArray];
+    [segmentedControl addTarget:self action:@selector(textStyleSelected:) forControlEvents:UIControlEventValueChanged];
+    segmentedControl.frame = CGRectMake(0, 0, 150.f, 30);
+    segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
+    segmentedControl.selectedSegmentIndex = 2;
+    segmentedControl.momentary = YES;
+    UIBarButtonItem *segmentedControlButtonItem = [[UIBarButtonItem alloc] initWithCustomView:(UIView *)segmentedControl];
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    NSArray *barArray = [NSArray arrayWithObjects: flexibleSpace, segmentedControlButtonItem, nil];
+    
+    if (currentBoldStatus != boldEnabled || currentItalicStatus != italicEnabled || currentUnderlineStatus != underlineEnabled || sender == self) {
+        self.editingToolbar.items = barArray;
+        currentBoldStatus = boldEnabled;
+        currentItalicStatus = italicEnabled;
+        currentUnderlineStatus = underlineEnabled;
+    }
+}
+
+#pragma mark - Actions
+
+- (void)textStyleSelected:(UISegmentedControl *)control
+{
+    switch (control.selectedSegmentIndex) {
+        case TextStyleBold:
+            [self bold];
+            break;
+            
+        case TextStyleItalic:
+            [self italic];
+            break;
+            
+        case TextStyleUnderline:
+            [self underline];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)bold {
+    [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand(\"Bold\")"];
+}
+
+- (void)italic {
+    [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand(\"Italic\")"];
+}
+
+- (void)underline {
+    [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand(\"Underline\")"];
 }
 
 #pragma mark - Split view
