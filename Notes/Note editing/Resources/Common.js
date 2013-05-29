e228c35b8da9a07de7108ff0b1d5f9adfa8fd550
nessup <nessup@gmail.com>
2013-05-29 02:41:06 -0700

    Added dragging

--- Notes/Note editing/Resources/Common.js
+++ Notes/Note editing/Resources/Common.js
@@ -101,7 +101,10 @@ function createResizableDraggableImageElement(src,width,height) {
         'nw': '#nwgrip'
         }
     });
-    imgObject.draggable();
+    imgObject.draggable({
+        snap:"#contentContainer",
+        scroll: true
+    });
 
     return imgObject;
 }
